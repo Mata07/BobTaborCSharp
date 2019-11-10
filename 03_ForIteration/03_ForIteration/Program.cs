f7ea86c8fc86beeda9c15189a0162a08701a8787
Matija Matisic <mmatisic7@gmail.com>
2019-11-11 00:24:07 +0100

    added first example in 03_ForIteration

--- 03_ForIteration/03_ForIteration/Program.cs
+++ 03_ForIteration/03_ForIteration/Program.cs
@@ -10,6 +10,11 @@ namespace _03_ForIteration
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
         }
     }
 }
