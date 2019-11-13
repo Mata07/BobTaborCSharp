40116be373d81884273cf6fa33a88d954bd2d661
Matija Matisic <mmatisic7@gmail.com>
2019-11-14 00:49:32 +0100

    added WorkingWithStrings project

---
+++ 12_WorkingWithStrings/WorkingWithStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // StringBuilder uses less runtime so it's better
+            StringBuilder myString = new StringBuilder();
+
+            for (int i = 0; i < 100; i++)
+            {
+                myString.Append("--");
+                myString.Append(i);
+            }
+
+            Console.WriteLine(myString);
+            Console.ReadLine();
+        }
+    }
+}
