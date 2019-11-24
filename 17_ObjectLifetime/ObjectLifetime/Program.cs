5fff4edbfc2fc06309b1556b2a275afcd78e6071
Matija Matisic <mmatisic7@gmail.com>
2019-11-24 02:19:44 +0100

    finished ObjectLifetime project

--- 17_ObjectLifetime/ObjectLifetime/Program.cs
+++ 17_ObjectLifetime/ObjectLifetime/Program.cs
@@ -12,10 +12,12 @@ namespace ObjectLifetime
         {
             Car myCar = new Car();
 
+            /*
             myCar.Make = "Oldsmobile";
             myCar.Model = "Cutlass Supreme";
             myCar.Year = 1986;
             myCar.Color = "Silver";
+            */
 
             // Second reference to the same Object
             Car myOtherCar;
@@ -38,6 +40,35 @@ namespace ObjectLifetime
                 myCar.Color
                 );
 
+            // null (indeteriment) example
+            //removing reference to Object intetionally
+
+            //myOtherCar = null;
+
+            ////Console.WriteLine("{0} {1} {2} {3}",
+            ////    myOtherCar.Make,
+            ////    myOtherCar.Model,
+            ////    myOtherCar.Year,
+            ////    myOtherCar.Color
+            ////    );
+
+            //myCar = null;
+
+
+            #region Constructor exmpl
+            //3. example, Overloaded constructor
+            Car myThirdCar = new Car("Ford", "Escape", 2005, "White");
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myThirdCar.Make,
+                myThirdCar.Model,
+                myThirdCar.Year,
+                myThirdCar.Color
+                );
+            #endregion
+
+            Car.MyMethod();
+
             Console.ReadLine();
         }
     }
@@ -48,5 +79,24 @@ namespace ObjectLifetime
         public string Model { get; set; }
         public int Year { get; set; }
         public string Color { get; set; }
+
+        public Car()
+        {
+            // this = getting a member of this class
+            this.Make = "Nissan";
+        }
+
+        public Car(string make, string model, int year, string color)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+
+        public  static void MyMethod()
+        {
+            Console.WriteLine("Called the static MyMethod");
+        }
     }
 }
