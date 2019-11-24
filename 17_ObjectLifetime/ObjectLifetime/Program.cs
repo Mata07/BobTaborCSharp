1dfa9e0085226a0436f62707b0f018e2872192f6
Matija Matisic <mmatisic7@gmail.com>
2019-11-24 02:00:12 +0100

    added first example

--- 17_ObjectLifetime/ObjectLifetime/Program.cs
+++ 17_ObjectLifetime/ObjectLifetime/Program.cs
@@ -12,6 +12,31 @@ namespace ObjectLifetime
         {
             Car myCar = new Car();
 
+            myCar.Make = "Oldsmobile";
+            myCar.Model = "Cutlass Supreme";
+            myCar.Year = 1986;
+            myCar.Color = "Silver";
+
+            // Second reference to the same Object
+            Car myOtherCar;
+            // copy address of myCar into myOtherCar, point to it
+            myOtherCar = myCar;
+
+            Console.WriteLine("{0} {1} {2} {3}", 
+                myOtherCar.Make,
+                myOtherCar.Model,
+                myOtherCar.Year,
+                myOtherCar.Color
+                );
+
+            myOtherCar.Model = "98";
+
+            Console.WriteLine("{0} {1} {2} {3}",
+                myCar.Make,
+                myCar.Model,
+                myCar.Year,
+                myCar.Color
+                );
 
             Console.ReadLine();
         }
@@ -20,7 +45,7 @@ namespace ObjectLifetime
     class Car
     {
         public string Make { get; set; }
-        public string Mode { get; set; }
+        public string Model { get; set; }
         public int Year { get; set; }
         public string Color { get; set; }
     }
